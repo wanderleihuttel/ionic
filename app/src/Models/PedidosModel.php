dd655abff1c869132876801100e959e2277ab4e6
Jonathan Silva <jteccondeuba@hotmail.com>
2017-04-26 23:43:52 -0300

    API

--- app/src/Models/PedidosModel.php
+++ app/src/Models/PedidosModel.php
@@ -12,10 +12,13 @@
 						`loja_pedidos`.`id_loja`,
 						`loja_pedidos`.`status`,
 						`loja_produtos`.`nome` as nome_produto,
-						`loja_produtos`.`foto`
+						`loja_produtos`.`foto`,
+						`admin_lojas`.`nome` as nome_loja,
+						`admin_lojas`.`rua` as rua_loja
 							FROM `loja_produtos_pedidos`
 								INNER JOIN `loja_pedidos` ON (`loja_pedidos`.`id` = `loja_produtos_pedidos`.`id_pedido`)
 								INNER JOIN `loja_produtos` ON (`loja_produtos`.`id` = `loja_produtos_pedidos`.`id_produto`)
+								INNER JOIN `admin_lojas` ON (`admin_lojas`.`id` = `loja_pedidos`.`id_loja`)
 
 							WHERE `loja_pedidos`.`id_cliente` = :cliente
 				");
@@ -29,15 +32,14 @@
         }
 		
 		public function detalhes($id, $loja){
-			try {
+			try {				
 				$stmt = $this->db->prepare("
 					SELECT
-						`admin_lojas`.`nome`,
 						`loja_fotos_produto`.`foto`
 							FROM `loja_produtos_pedidos`
+								INNER JOIN `admin_lojas` ON (`admin_lojas`.`id` = `loja_produtos_pedidos`.`id_loja`)
 								INNER JOIN `loja_pedidos` ON (`loja_pedidos`.`id` = `loja_produtos_pedidos`.`id_pedido`)
 								INNER JOIN `loja_produtos` ON (`loja_produtos`.`id` = `loja_produtos_pedidos`.`id_produto`)
-								INNER JOIN `admin_lojas` ON (`admin_lojas`.`id` = `loja_produtos_pedidos`.`id_loja`)
 								INNER JOIN `loja_fotos_produto` ON (`loja_fotos_produto`.`id_produto` = `loja_produtos`.`id`)
 
 							WHERE `loja_pedidos`.`id` = :pedido AND `admin_lojas`.`id` = :loja
@@ -45,6 +47,7 @@
 				$stmt->bindValue(':pedido', $id, PDO::PARAM_INT);
 				$stmt->bindValue(':loja', $loja, PDO::PARAM_INT);
 				$stmt->execute();
+				
 				return $stmt->fetchAll(PDO::FETCH_ASSOC);
 			} catch(PDOException $e) {
 				die($e->getMessage());
