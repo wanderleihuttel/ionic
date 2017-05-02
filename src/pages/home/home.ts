f115751fbd89d1a9041ea06de2afe5312e00f8db
Jonathan Silva <jteccondeuba@hotmail.com>
2017-05-01 22:54:48 -0300

    Login

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, ActionSheetController, AlertController } from 'ionic-angular';
+import { NavController, ActionSheetController, AlertController, ToastController } from 'ionic-angular';
 
 import { Login } from '../login/login';
 import { Pedidos } from '../pedidos/pedidos';
@@ -12,13 +12,14 @@ import { PesquisaService } from '../../providers/pesquisa-service';
   templateUrl: 'home.html'
 })
 export class Home {
-  public showSearchBar: boolean = false;    
+  showSearchBar: boolean = false;  
+  pesquisa: string = '';
+    
   usuario: {};
+    
+  lojas: any[];
 
-  
-  lojas: any;
-
-  constructor(public nav: NavController, public actionSheetCtrl: ActionSheetController, public alertCtrl: AlertController, public service: PesquisaService) {
+  constructor(public nav: NavController, public actionSheetCtrl: ActionSheetController, public alertCtrl: AlertController, public toastCtrl: ToastController, public service: PesquisaService) {
     this.usuario = JSON.parse(localStorage.getItem('usuario'));
   }
 
@@ -83,14 +84,16 @@ export class Home {
   public loja(input) {
     let loja = input.target.value;
 
-    if (loja.length >= 4) {
-      this.service.pesquisa(this.jsonToURLEncoded({
-        nome: loja
-      })).subscribe(retorno => {
-        this.lojas = retorno;
-      }, error => {
-        console.log(error);
-      });   
+    if (loja && loja.trim() != '') {
+      if (loja.length >= 4) {
+        this.service.pesquisa(this.jsonToURLEncoded({
+          nome: loja
+        })).subscribe(retorno => {
+          this.lojas = retorno.loja;
+        }, error => {
+          this.alerta('Erro ao realizar busca, tente novamente');
+        });   
+      } 
     }
   }
     
@@ -102,5 +105,16 @@ export class Home {
     return Object.keys(jsonString).map(function(key) {
       return encodeURIComponent(key) + '=' + encodeURIComponent(jsonString[key]);
     }).join('&');
+  }
+
+  // Toast
+  alerta(mensagem) {
+    let toast = this.toastCtrl.create({
+      message: mensagem,
+      duration: 3000,
+      position: 'bottom'
+    });
+
+    toast.present();
   }
 }
