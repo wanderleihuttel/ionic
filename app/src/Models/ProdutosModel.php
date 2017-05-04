cbd293b722051fdeaa946a02464ac783842f4d73
Jonathan Silva <jteccondeuba@hotmail.com>
2017-05-03 23:49:18 -0300

    API

--- app/src/Models/ProdutosModel.php
+++ app/src/Models/ProdutosModel.php
@@ -7,14 +7,14 @@
 		public function produtos($loja){
 			$retorno['resposta'] = 'erro';
 			
-			$stmt = $this->db->prepare("SELECT `id`, `nome` FROM `loja_produtos` WHERE `id_loja` = :loja");
+			$stmt = $this->db->prepare("SELECT `id`, `id_loja`, `foto` as foto_produto, `nome` as nome_produto, `descricao`, `preco`, `estoque` FROM `loja_produtos` WHERE `id_loja` = :loja");
 			$stmt->bindValue(':loja', $loja, PDO::PARAM_INT);
 			$stmt->execute();
 			
-			$dados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			$produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
 			
-			if (count($dados) > 0) {
-				$retorno = array('resposta' => 'ok', 'resposta' => $dados);
+			if (count($produtos) > 0) {
+				$retorno = array('resposta' => 'ok', 'produtos' => $produtos);
 			}
 			return $retorno;
 		}
