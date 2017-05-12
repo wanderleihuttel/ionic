cd77b569502d974cac37122bdd50b8cada07212a
Jonathan Silva <jteccondeuba@hotmail.com>
2017-05-12 14:18:19 -0300

    API

--- app/src/Models/ProdutosModel.php
+++ app/src/Models/ProdutosModel.php
@@ -33,7 +33,7 @@
 			return $retorno;
 		}
 		
-		public function produtos_categoria($estabelecimento, $categoria){
+		/*public function produtos_categoria($estabelecimento, $categoria){
 			$retorno['resposta'] = 'erro';
 			
 			$stmt = $this->db->prepare("SELECT `id`, `id_loja`, `foto`, `nome`, `descricao`, `preco`, `estoque` FROM `loja_produtos` WHERE `id_loja` = :loja AND `categoria` = :categoria");
@@ -47,24 +47,5 @@
 				$retorno = array('produtos' => $produtos);
 			}
 			return $retorno;
-		}
-		
-		public function carregar_mais($estabelecimento, $quantidade){
-			$retorno['resposta'] = 'erro';
-			
-			$stmt = $this->db->prepare("
-				SELECT `id`, `id_loja`, `foto`, `nome`, `descricao`, `preco`, `estoque`
-				FROM `loja_produtos` WHERE `id_loja` = :loja LIMIT 2
-			");
-			$stmt->bindValue(':loja', $estabelecimento, PDO::PARAM_INT);
-			$stmt->bindValue(':quantidade', $quantidade, PDO::PARAM_INT);
-			$stmt->execute();
-			
-			$produtos = $stmt->fetchAll(PDO::FETCH_ASSOC);
-			
-			if (count($produtos) > 0) {
-				$retorno = array('produtos' => $produtos);
-			}
-			return $retorno;
-		}
+		}*/
 	}
